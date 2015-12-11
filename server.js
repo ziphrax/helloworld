d06862d35693ef771b3251f4742c34cbb047b8ad
David Jones david.jones@mso.net <david.jones@mso.net>
2015-12-11 13:08:48 +0000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express'),
+app = express();
+
+app.get('/',function(req,res){
+    res.send('Hello World!');
+});
+
+app.listen(3000,function(){
+    console.log('app listening on port ' + 3000);
+});
